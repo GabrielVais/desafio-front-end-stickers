3b80a448eec35e1d137194a2ed4e2e6f757dee87
gabrielVais <gabriel_lol2017@hotmail.com>
2019-08-27 14:10:57 -0300

    finalizado checkbox / contador de stickers / funçao contador de stickers javascript

---
+++ js/functions.js
@@ -0,0 +1,51 @@
+
+var inputValue = document.querySelector("#sticker");
+
+var valor = 0;
+
+var btnLess = document.querySelector("#btnLess");
+
+var btnAdd = document.querySelector("#btnAdd");
+
+
+
+function addSticker(){
+
+	inputValue.value = isNaN(inputValue.value ) ? 0 : inputValue.value;
+
+    valor++;
+
+
+    inputValue.value = valor
+
+}
+
+
+function removeSticker(){
+
+	if(inputValue.value > 0){
+
+    inputValue.value = isNaN(inputValue.value) ? 0 : inputValue.value;
+    
+    valor--;
+
+    inputValue.value = valor;
+
+	}
+
+}
+
+
+
+btnLess.addEventListener("click", function(){
+
+	removeSticker();
+
+})
+
+
+btnAdd.addEventListener("click", function(){
+
+	addSticker();
+
+})
